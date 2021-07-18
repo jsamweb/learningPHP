b14c2c009e01e47a96e6dc57428ab70085b983ff
jsamweb <jsam.web@gmail.com>
2021-07-18 15:57:42 +0200

    v1.0 aprendiendo como funcniona el autoload

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+require __DIR__ . "/vendor/autoload.php"; //siempre que usemos un archivo principal debemos invocar al archivo de autocargas "autoload" ya que eso es lo que realmente es util 
