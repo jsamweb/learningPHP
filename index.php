80c582b2f1c423c226a09655c5ca9f077ecf672d
jsamweb <jsam.web@gmail.com>
2021-07-20 22:18:35 +0200

    v1.0 formato de datos

--- index.php
+++ index.php
@@ -1,27 +1,52 @@
 <?php
-
-// $data = "estudio PHP";
-// echo $data[0];
-
-// $post = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem aut dolores soluta eaque impedit consequuntur dolore architecto neque ab repellendus, animi at facere eius, sunt autem provident optio nemo eum!";
-// $extract = substr($post, 0, 60);
-
-// echo "$extract... [chupapimuñaño]";
-
-//pasar de un string a un array
-$data = "javas, php, elguebo, chapulin"; //campo tags
-$tags = explode(",", $data); //array
+#Alterar
+$text = "<h1>PHP es UN LENGUAJE</h1>";
+echo $text;
+//Hacer un texto en minuscula
 echo "<pre>";
-var_dump($tags);
-
-//pasasr de un array a un string
-$courses = ["javas", "guebo","xyz"];
-echo implode(",", $courses);
-
-
-//example real
-
-$course = "   course de PHP   ";
-$course = trim($course);
+echo strtolower($text);
+//Hacer un texto en Mayuscula
 echo "<pre>";
-echo "quiero aprender $course , sdnfoisdjfposd";
+echo strtoupper($text);
+//Convierte el primer elemento en minuscula
+echo "<pre>";
+echo lcfirst($text);
+//Convierte el primer elemento en Mayuscula
+echo "<pre>";
+echo ucfirst($text);
+
+echo "<br><br>";
+#Reemplazar
+//reemplazamos cada espacio vacio por un guión(-)
+$slug = str_replace(' ', '-', $text);
+echo $slug;
+echo strtolower($slug);
+
+echo "<br><br>";
+#Modificación
+//Si tenemos una factura se imprima de la siguiente forma.
+$code = 39;
+//Vamos a tener 8 espacio con 8 carácteres
+echo "<br>";
+echo str_pad($code, 8, '#');
+echo "<br>";
+//Ambos lados, izquierda y derecha
+echo str_pad($code, 8, '#', STR_PAD_BOTH);
+echo "<br>";
+echo str_pad($code, 8, '#', STR_PAD_LEFT);
+echo "<br>";
+echo str_pad($code, 8, '#', STR_PAD_RIGHT);
+
+echo "<br><br>";
+//En nuestra base de datos no se deben de guardar etiquetas.
+//Con strip_tags eliminamos cualquier etiqueta html que contega el dato
+echo strip_tags($text);
+
+echo "<br><br>";
+#ELEMENTOS MONOBYTES Y MULTIBYTES.
+$text_uno = "PHP es UN LENGUAJE, año 2020, programación";
+$text_dos = "PHP es UN LENGUAJE, año 2020, programación";
+echo strtoupper($text_uno);//monobye
+echo "<br>";
+//Al colocar mb_nombrefuncion con esto formateamos correctamente.
+echo mb_strtoupper($text_dos);//multibyte
