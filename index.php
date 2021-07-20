572663d746fa0ba18f0c3c35ee8ac41be2a360ed
jsamweb <jsam.web@gmail.com>
2021-07-20 18:14:47 +0200

    uso de comilla en PHP

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+// echo "esto es un texto";
+
+// $name = "Jesusito";
+
+// echo "mi nombre es $name";
+
+// $courses = [
+//     "backend" => [
+//         "php",
+//         "laravel"
+//     ]
+// ];
+
+// class Clase
+// {
+//     public $name = "sebistron";
+// }
+
+// $objeto = new Clase;
+
+// echo "$objeto->name quiere aprender {$courses["backend"][0]}";
+
+// $xyz = "primera";
+// $variableSecundaria = "segundo";
+// echo "$xyz es ${$xyz}";
+
+function getXyz()
+{
+    return "groseria";
+}
+
+$groseria = "que es esto";
+
+echo "${getXyz()} enseña el bowe";
