b2371532143ddfeb3df09d4f71c0310b88413e0a
jsamweb <jsam.web@gmail.com>
2021-07-20 23:17:30 +0200

    v1.0 expresiones regulares

--- index.php
+++ index.php
@@ -1,52 +1,25 @@
 <?php
-#Alterar
-$text = "<h1>PHP es UN LENGUAJE</h1>";
-echo $text;
-//Hacer un texto en minuscula
-echo "<pre>";
-echo strtolower($text);
-//Hacer un texto en Mayuscula
-echo "<pre>";
-echo strtoupper($text);
-//Convierte el primer elemento en minuscula
-echo "<pre>";
-echo lcfirst($text);
-//Convierte el primer elemento en Mayuscula
-echo "<pre>";
-echo ucfirst($text);
 
-echo "<br><br>";
-#Reemplazar
-//reemplazamos cada espacio vacio por un guión(-)
-$slug = str_replace(' ', '-', $text);
-echo $slug;
-echo strtolower($slug);
+#EXPRESIONES REGULARES
+/*
+/:  CONTENEDOR
+^: INICIO
+$: FINAL
+-: RANGO
+[]: PATRÓN 
+{}: CONDICIÓN
+*/
 
-echo "<br><br>";
-#Modificación
-//Si tenemos una factura se imprima de la siguiente forma.
-$code = 39;
-//Vamos a tener 8 espacio con 8 carácteres
+$password = '12345';
+$password1 = '123456a';
+$password2 = '123456';
+//Iniciamos con un contenedo // luego iniciamos con  ^ y que solo permita un patrón del 0 al 9 [0-9] 
+//la condicion en llaves y que tenga como minimo 6 elementos y máximo 9 caracteres {6,9}
+echo preg_match('/^[0-9]{6,9}$/', $password);
+//Resultado 0
 echo "<br>";
-echo str_pad($code, 8, '#');
+var_dump(preg_match('/^[0-9]{6,9}$/', $password1));
+//Resultado int(0)
 echo "<br>";
-//Ambos lados, izquierda y derecha
-echo str_pad($code, 8, '#', STR_PAD_BOTH);
-echo "<br>";
-echo str_pad($code, 8, '#', STR_PAD_LEFT);
-echo "<br>";
-echo str_pad($code, 8, '#', STR_PAD_RIGHT);
-
-echo "<br><br>";
-//En nuestra base de datos no se deben de guardar etiquetas.
-//Con strip_tags eliminamos cualquier etiqueta html que contega el dato
-echo strip_tags($text);
-
-echo "<br><br>";
-#ELEMENTOS MONOBYTES Y MULTIBYTES.
-$text_uno = "PHP es UN LENGUAJE, año 2020, programación";
-$text_dos = "PHP es UN LENGUAJE, año 2020, programación";
-echo strtoupper($text_uno);//monobye
-echo "<br>";
-//Al colocar mb_nombrefuncion con esto formateamos correctamente.
-echo mb_strtoupper($text_dos);//multibyte
+var_dump((bool)preg_match('/^[0-9]{6,9}$/', $password2));
+//Resultado bool(true)
