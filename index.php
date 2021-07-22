cc779b269b8268d9d24040c83890f1b2ee59ae39
jsamweb <jsam.web@gmail.com>
2021-07-22 18:22:41 +0200

    aprendiendo un poco sobre argumentos

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
 
-require __DIR__ . "vendor/autoload.php";
+// // forma basica de trabajar con valores
+//  function greet($name)
+// {
+//     return "hola, $name";
+// }
 
-var_dump(App\validate::email("email@email.com"));
+// echo greet("sebas");
 
-#EXPRESIONES REGULARES
-/*
-/:  CONTENEDOR
-^: INICIO
-$: FINAL
--: RANGO
-[]: PATRÓN 
-{}: CONDICIÓN
-*/
+// //referencias
+// $course = "PHP";
+// function path(&$course)
+// {
+//     $course = "laravel";
+//     echo $course; //laravel
+// }
 
-// $password = '12345';
-// $password1 = '123456a';
-// $password2 = '123456';
-// //Iniciamos con un contenedo // luego iniciamos con  ^ y que solo permita un patrón del 0 al 9 [0-9] 
-// //la condicion en llaves y que tenga como minimo 6 elementos y máximo 9 caracteres {6,9}
-// echo preg_match('/^[0-9]{6,9}$/', $password);
-// //Resultado 0
-// echo "<br>";
-// var_dump(preg_match('/^[0-9]{6,9}$/', $password1));
-// //Resultado int(0)
-// echo "<br>";
-// var_dump((bool)preg_match('/^[0-9]{6,9}$/', $password2));
-// //Resultado bool(true)
+// path($course);
+// echo $course; //laravel
+
+
+//predeterminado
+function greet($name = "sebas")
+{
+    return "Hola, $name";
+}
+
+echo greet(); // de esta manera saldria el nombre predetermiando en la funcion
+echo greet("perro"); //de esta manera se modifica el nombre interno de la funcion
+
