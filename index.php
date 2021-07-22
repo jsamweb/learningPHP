4c6116e3b6c82e27a6a88fec38b5d3dcc9df7473
jsamweb <jsam.web@gmail.com>
2021-07-22 17:54:33 +0200

    proyecto finalizado

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+require __DIR__ . "vendor/autoload.php";
+
+var_dump(App\validate::email("email@email.com"));
+
 #EXPRESIONES REGULARES
 /*
 /:  CONTENEDOR
@@ -10,16 +14,16 @@ $: FINAL
 {}: CONDICIÓN
 */
 
-$password = '12345';
-$password1 = '123456a';
-$password2 = '123456';
-//Iniciamos con un contenedo // luego iniciamos con  ^ y que solo permita un patrón del 0 al 9 [0-9] 
-//la condicion en llaves y que tenga como minimo 6 elementos y máximo 9 caracteres {6,9}
-echo preg_match('/^[0-9]{6,9}$/', $password);
-//Resultado 0
-echo "<br>";
-var_dump(preg_match('/^[0-9]{6,9}$/', $password1));
-//Resultado int(0)
-echo "<br>";
-var_dump((bool)preg_match('/^[0-9]{6,9}$/', $password2));
-//Resultado bool(true)
+// $password = '12345';
+// $password1 = '123456a';
+// $password2 = '123456';
+// //Iniciamos con un contenedo // luego iniciamos con  ^ y que solo permita un patrón del 0 al 9 [0-9] 
+// //la condicion en llaves y que tenga como minimo 6 elementos y máximo 9 caracteres {6,9}
+// echo preg_match('/^[0-9]{6,9}$/', $password);
+// //Resultado 0
+// echo "<br>";
+// var_dump(preg_match('/^[0-9]{6,9}$/', $password1));
+// //Resultado int(0)
+// echo "<br>";
+// var_dump((bool)preg_match('/^[0-9]{6,9}$/', $password2));
+// //Resultado bool(true)
